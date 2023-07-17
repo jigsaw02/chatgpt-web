0de3a09532800919b6093948b95fe728853d0e70
junfeng.hu <junfeng.hu@test.com>
2023-07-17 20:03:12 +0800

    v1.0.0

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <span>powered by</span>
+    <a href="https://gptx.chatgptten.com/" target="_blank" class="text-blue-500">
+			GPT
     </a>
   </div>
 </template>
